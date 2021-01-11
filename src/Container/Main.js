255765f35edd0c720d0d923705c245379798f9b5
rameezamemon1 <rameeza.memon1@gmail.com>
2021-01-11 18:26:26 +0500

    home page done

--- src/Container/Main.js
+++ src/Container/Main.js
@@ -6,15 +6,22 @@ import Skills from '../Components/skills';
 import Footer from '../Components/footer';
 import Services from '../Components/services';
 import Portfolio from '../Components/portfolio';
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 import '../index.css'
 
 
 
 function Main() {
+  useEffect(() => {
+    Aos.init({ duration: 2000, offset: 200 });
+  }, []);
     return (
       <div className="main-container">
           <Nav />
-          {/* <Home /> 
+          <Home /> 
+          {/*
           <Services />
           <Skills />
           <Portfolio />
